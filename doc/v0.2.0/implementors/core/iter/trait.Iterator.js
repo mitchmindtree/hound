36eb2a5576c60713b0e03fc66c2f3f79b2c8012d
Ruud van Asseldonk <dev@veniogames.com>
2015-05-16 01:13:04 +0200

    add Hound v0.2.0 API reference

---
+++ doc/v0.2.0/implementors/core/iter/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['hound'] = ["impl&lt;'wr, R, S&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='hound/struct.WavSamples.html' title='hound::WavSamples'>WavSamples</a>&lt;'wr, R, S&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
