ae84425c0c6d95e4e0d114528481a1902683c447
Ruud van Asseldonk <dev@veniogames.com>
2015-05-16 01:14:50 +0200

    add Hound v0.3.0 API reference

---
+++ doc/v0.3.0/implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['hound'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='hound/struct.WavSpec.html' title='hound::WavSpec'>WavSpec</a>","impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='enum' href='hound/enum.Error.html' title='hound::Error'>Error</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
