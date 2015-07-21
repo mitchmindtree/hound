7d9e73da0b602f9574c2f37d0fa0685ae2e29c2c
Ruud van Asseldonk <dev@veniogames.com>
2015-07-21 17:21:59 +0200

    add Hound v1.0.0 API reference

---
+++ doc/v1.0.0/implementors/core/iter/trait.ExactSizeIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['hound'] = ["impl&lt;'wr, R, S&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.ExactSizeIterator.html' title='core::iter::ExactSizeIterator'>ExactSizeIterator</a> for <a class='struct' href='hound/struct.WavSamples.html' title='hound::WavSamples'>WavSamples</a>&lt;'wr, R, S&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/v1.0.0/implementors/core/marker/trait.Copy.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['hound'] = ["impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Copy.html' title='core::marker::Copy'>Copy</a> for <a class='struct' href='hound/struct.WavSpec.html' title='hound::WavSpec'>WavSpec</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
